de43d0aca2813121b0e257a10c905369d7fe87a4
Thanvireddy23 <24wh1a6651@bvrithyderabad.edu.in>
2026-06-20 10:32:45 +0530

    created randomuser

---
+++ frontend/js/randomuser.js
@@ -0,0 +1,19 @@
+function randomuser(){
+    fetch("https://randomuser.me/api/")
+        .then(function(res){
+            return res.json();
+
+        })
+        .then(function(data){
+            var user = data.results[0];
+            var fullName = user.name.title +" " + user.name.first + " " + user.name.last;
+            document.getElementById("user-img").src=user.picture.large;
+    //update name and gender
+            document.getElementById("user-name").innerText=fullName;
+            document.getElementById("user-gender").innerText=user.gender;
+
+        })
+        .catch(function(error){
+            console.log("Error : " +err);
+        })
+}
